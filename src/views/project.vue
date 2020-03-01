27e54ca199ddbddb67942fc9f07747f0ee871a9b
赵愉 <yuzhao214960@souhu-inc.com>
2020-03-01 20:19:12 +0800

    改图片引入方式

--- src/views/project.vue
+++ src/views/project.vue
@@ -29,126 +29,8 @@
 </div>
 </template>
 <script>
-const imgNumber = [13, 18, 13, 8, 15, 13, 8, 11, 9]
-const desc = [
-  {
-    description: 'STUDIO 715设计工作室为ILOVEPRETTY设计了全新的店铺空间，<br/>' +
-      '以大胆前卫的色彩和空间语言来重新诠释ILOVEPRETTY的时尚魅力。<br/>' +
-      '受著名意大利画家Giorgio De Chirico画作的影响，<br/>' +
-      '空间中采用了拱形门廊来分割空间，增加空间的层次感。<br/>' +
-      '同时空间中大量运用新奇有趣的几何元素，<br/>' +
-      '如整面墙的折线形墙板随着人的移动而形成不同的光影效果，<br/>' +
-      '具有积木形态的几何中岛，撞色的设计颇具视觉冲击力，<br/>' +
-      '仿佛引领人们来到一个奇妙梦幻的游乐场。',
-    location:  'LOCATION：<span font-weight: 600">深圳中心城</span><br/>' +
-      'YEAR：2018<br/>' +
-      'PHOTOGRAPHY：STUDIO 715'
-  },
-  {
-    description: 'STUDIO 715设计工作室为ThoughtWorks思特沃克·北京设计了全新办公体验的办公空间，<br/>' +
-      '用设计语言更好的诠释了ThoughtWorks本身的企业文化。<br/>' +
-      'ThoughtWorks鼓励员工之间的沟通交流，活跃的分享，希望拥有一个相对开放、平等、自由的办公环境。<br/>' +
-      '我们希望通过对北京办公室1700平米空间的重新思考，打造一个<br/>' +
-      '极富通透感的无阻隔的交流空间，提升员工的办公体验，<br/>' +
-      '同时传达公司先进的企业文化和管理理念。<br/>',
-    location: 'LOCATION： <span font-weight: 600">北京</span><br/>' +
-      'YEAR：2018 <br/>' +
-      'PHOTOGRAPHY：STUDIO 715 <br/>'
-  },
-  {
-    description: 'WF+鞋履配饰集合店 (World Footwear Plus)是一家<br/>' +
-      '以意大利手工皮鞋品牌以及高端私人定制为主，<br/>' +
-      '融合了时尚服饰、配饰的生活体验馆。<br/>' +
-      '空间中结合了中性时尚的水泥灰与纤细优雅的复古黄铜色金属线条，<br/>' +
-      '打造一个简约现代的精品形象，<br/>' +
-      '凸显产品本身的手工质感以及纯正的色彩。',
-    location: 'LOCATION： <span font-weight: 600">北京合生汇</span> <br/>' +
-      'YEAR：2018 <br/>' +
-      'PHOTOGRAPHY：STUDIO 715'
-  },
-  {
-    description: 'STUDIO 715为鞋履配饰品牌集合店 WF+ （World Footwear Plus）展厅设计了全新的陈列展示。<br/>' +
-      '整体陈列风格以现代感极强的几何色块为主，<br/>' +
-      '搭配家具简洁流畅的线条，搭建出几种色彩主题的陈列背景。<br/>' +
-      '每个场景利用墙面层板、小沙发、地毯、边桌等软装家具<br/>' +
-      '作为鞋履陈列的承载物，<br/>' +
-      '色彩明快、主次分明，体现一种现代趣味的生活理念，让鞋、包等配饰在生活场景中鲜明、活泼起来。',
-    location: 'LOCATION： <span font-weight: 600">北京</span> <br/>' +
-      'YEAR：2017 <br/>' +
-      'PHOTOGRAPHY：STUDIO 715'
-  },
-  {
-    description:'法国品牌让·路易·雪莱Jean-Louis Scherrer诞生于1962年， <br/>' +
-    '是一家集时装、配饰、香水等一体的高级品牌。<br/>' +
-    '为体现其年轻、时尚、优雅、高级的服装品质，我们采用了<br/>' +
-    '来自意大利的Arabescato Corchia（网纹大花白）<br/>' +
-    '大理石与经典的黄铜元素相结合，用清爽的石材调和时下最流行的复古金属感。<br/>' +
-    '色彩简约，线条利落，体现意式设计的低调奢华。',
-    location: 'LOCATION： <span font-weight: 600">广州、苏州</span> <br/>' +
-      'YEAR：2016 <br/>' +
-      'PHOTOGRAPHY：STUDIO 715'
-  },
-  {
-    description:'STUDIO 715为ThoughtWorks思特沃克·武汉设计了崭新的办公空间。 <br/>' +
-    '不同于传统的办公空间，<br/>' +
-    '我们在办公室中创造了一个又一个“交汇点”——In-between Space。<br/>' +
-    '通过地面材质色彩的变化，这些交汇点一方面为斯特沃克的员工提供非正式的讨论、会议环境，<br/>' +
-    '另一方面也担任了衔接各个不同功能空间的角色。<br/>' +
-    '开放办公区的办公桌的摆放呈Z字形，制造更多的交汇点和路线，<br/>' +
-    '，在办公室活动中增加人与人之间相遇的几率，<br/>' +
-    '给枯燥的办公环境带来更加亲切自然的舒适感。<br/>' +
-    '深色落地玻璃隔断从空间上分割了公共过道和工作区，<br/>' +
-    '同时可以作为员工小组讨论的写字板。<br/>' +
-    '用餐区和阅读区采用了更明亮的色彩。<br/>' +
-    '高脚椅、餐椅、懒人沙发为员工提供了多种休息和聚会的方式。',
-    location: 'LOCATION： <span font-weight: 600">武汉</span> <br/>' +
-      'YEAR：2019 <br/>' +
-      'PHOTOGRAPHY：ThoughtWorks'
-  },
-  {
-    description:'STUDIO 715为诺特钢琴音乐中心（NOTE PIANO）打造了崭新的艺术教育空间。 <br/>' +
-    '为了体现音乐教育的艺术特质，<br/>' +
-    'STUDIO715采用了曲线、弧形、拱门作为设计语言贯穿整个空间，<br/>' +
-    '打造如音乐旋律般柔和优雅的氛围，<br/>' +
-    '为小学员们提供一个充满梦幻气息的场景。同时，<br/>' +
-    '也期望以美学空间的角度帮助小朋友们从小建立良好的审美认知。<br/>' +
-    '灰色花纹大理石前台中镶嵌发光灯箱，如同点亮的两枚钢琴琴键。<br/>' +
-    '演奏中心的波纹不锈钢顶面倒映出波光粼粼的灯光效果，<br/>' +
-    '月球灯悬浮在主钢琴上方，使演奏者成为舞台的聚焦点。<br/>' +
-    '金色拱门衔接每一个不同的场景，仿佛在童话故事中穿梭而行。',
-    location: 'LOCATION： <span font-weight: 600">青岛 - 新城吾悦广场</span> <br/>' +
-      'YEAR：2019 <br/>' +
-      'PHOTOGRAPHY：NOTE PIANO'
-  },
-  {
-    description:'STUDIO 715为诺特钢琴音乐中心（NOTE PIANO）打造了崭新的艺术教育空间。 <br/>' +
-    '为了体现音乐教育的艺术特质，<br/>' +
-    'STUDIO715采用了曲线、弧形、拱门作为设计语言贯穿整个空间，<br/>' +
-    '打造如音乐旋律般柔和优雅的氛围，<br/>' +
-    '为小学员们提供一个充满梦幻气息的场景。同时，<br/>' +
-    '也期望以美学空间的角度帮助小朋友们从小建立良好的审美认知。<br/>' +
-    '灰色花纹大理石前台中镶嵌发光灯箱，如同点亮的两枚钢琴琴键。<br/>' +
-    '演奏中心的波纹不锈钢顶面倒映出波光粼粼的灯光效果，<br/>' +
-    '月球灯悬浮在主钢琴上方，使演奏者成为舞台的聚焦点。<br/>' +
-    '金色拱门衔接每一个不同的场景，仿佛在童话故事中穿梭而行。',
-    location: 'LOCATION： <span font-weight: 600">青岛 - 保利广场</span> <br/>' +
-    'YEAR：2019 <br/>' +
-    'PHOTOGRAPHY：NOTE PIANO'
-  },
-  {
-    description:'STUDIO 715为POISON潮牌买手店设计了崭新前卫的零售空间。 <br/>' +
-    '空间中高纯度的蓝色灵感来源于法国艺术家Yves Klein，<br/>' +
-    '波普艺术最重要的代表人物之一，特立独行的艺术先驱。<br/>' +
-    '著名的克莱因蓝IKB以他命名。<br/>' +
-    'Yves Klein相信，只有最单纯的色彩才能唤起最强烈的心灵感受力。<br/>' +
-    '我们使这种极致纯净的蓝色与银色锡纸墙面的金属光泽相互呼应，<br/>' +
-    '来创造一种极具科幻魅力的未来空间。<br/>' +
-    '时尚与艺术之间的相互影响，也是我们对艺术先行者的致敬。',
-    location: 'LOCATION： <span font-weight: 600">缅甸</span> <br/>' +
-      'YEAR：2019 <br/>' +
-      'PHOTOGRAPHY：POISON'
-  },
-]
+import { desc, images } from './constant';
+
 export default {
   data() {
     return {
@@ -198,24 +80,13 @@ export default {
         this.activeIndex++
       }
       this.clickItem(this.activeIndex, 'left')
-    },
-    _readFileName(filePath) {
-      console.log(filePath)
-
-      fs.readdir(filePath, (err, files) => {
-        files.forEach(filename => {
-          this.imgs.push(path.join(filePath, filename))
-        });
-      })
     }
   },
   created () {
-    let projectId = +this.$route.query.index
-    this.imgs = new Array(imgNumber[projectId]).fill().map((element, fileIndex) => {
-      this.description = desc[projectId].description
-      this.location = desc[projectId].location      
-      return require('../assets/' + projectId + '/' + (+fileIndex + 1) + '.png')
-    });
+    const projectId = +this.$route.query.index
+    this.description = desc[projectId].description
+    this.location = desc[projectId].location    
+    this.imgs = images[+projectId]
     this.activeImg = this.imgs[0]
   }
 }
